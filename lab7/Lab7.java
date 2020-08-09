bbe819065f6e6906481ce28006a4890c96952584
fatinnurliana <68184304+fatinnurliana@users.noreply.github.com>
2020-08-09 19:25:54 +0800

    Add files via upload

---
+++ lab7/Lab7.java
@@ -0,0 +1,15 @@
+/*    FATIN NUR LIANA BINTI MUSTAFFA KAMAL
+ *    BI19110149
+ *    KK14203 OOP SECTION 1 
+ * LAB 7
+ */
+ 
+ public class Lab7{
+      
+              public static void main(String[] args){
+              
+              new MyFrame();
+              
+              
+           }
+    }
